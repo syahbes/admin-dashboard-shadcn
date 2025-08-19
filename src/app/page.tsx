60b0eb6020ba2675bc387b718a4f95e6b4b25477
Shlomo Y <shlomo@techtio.io>
2025-08-19 13:51:02 +0300

    todo and calendar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import AppBarChart from "@/components/AppBarChart";
 import AppAreaChart from "@/components/AppAreaChart";
 import AppPieChart from "@/components/AppPieChart";
 import CardList from "@/components/CardList";
+import TodoList from "@/components/TodoList";
 
 const Homepage = () => {
     return (
@@ -15,7 +16,9 @@ const Homepage = () => {
             <div className="bg-primary-foreground p-4 rounded-lg">
                 <AppPieChart />
             </div>
-            <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+            <div className="bg-primary-foreground p-4 rounded-lg">
+                <TodoList />
+            </div>
             <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
                 <AppAreaChart />
             </div>
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,93 @@
+"use client";
+import React from "react";
+import { Card } from "@/components/ui/card";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Calendar } from "@/components/ui/calendar";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { Button } from "./ui/button";
+import { CalendarIcon } from "lucide-react";
+import { format } from "date-fns";
+
+const items = [
+    {
+        id: "item1",
+        label: "lorem ipsum dolor",
+        isChecked: true,
+    },
+    {
+        id: "item2",
+        label: "sit amet consectetur",
+        isChecked: false,
+    },
+    {
+        id: "item3",
+        label: "adipiscing elit sed",
+        isChecked: true,
+    },
+    {
+        id: "item4",
+        label: "do eiusmod tempor",
+        isChecked: false,
+    },
+    {
+        id: "item5",
+        label: "incididunt ut labore",
+        isChecked: true,
+    },
+    {
+        id: "item6",
+        label: "et dolore magna",
+        isChecked: false,
+    },
+    {
+        id: "item7",
+        label: "aliqua ut enim",
+        isChecked: true,
+    },
+];
+
+export default function todolist() {
+    const [date, setDate] = React.useState<Date | undefined>(new Date());
+    const [open, setOpen] = React.useState(false);
+
+    return (
+        <div className="">
+            <h1 className="text-lg font-medium mb-6">Todo List</h1>
+            <Popover open={open} onOpenChange={setOpen}>
+                <PopoverTrigger asChild>
+                    <Button className="w-full">
+                        <CalendarIcon />
+                        {date ? format(date, "PPP") : <span>Pick a date</span>}
+                    </Button>
+                </PopoverTrigger>
+                <PopoverContent className="p-0 w-auto">
+                    <Calendar
+                        mode="single"
+                        selected={date}
+                        onSelect={date => {
+                            setDate(date);
+                            setOpen(false);
+                        }}
+                    />
+                </PopoverContent>
+            </Popover>
+            {/* LIST */}
+            <ScrollArea className="max-h-[400px] mt-4 overflow-y-auto">
+                <div className="flex flex-col gap-4">
+                    {/* LIST ITEM */}
+                    {items.map(item => (
+                        <Card key={item.id} className="p-4">
+                            <div className="flex items-center gap-4">
+                                <Checkbox id={item.id} />
+                                <label htmlFor={item.id} className="text-sm text-muted-foreground">
+                                    {item.label}
+                                </label>
+                            </div>
+                        </Card>
+                    ))}
+                </div>
+            </ScrollArea>
+        </div>
+    );
+}
