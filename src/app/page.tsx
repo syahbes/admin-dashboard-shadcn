55f5d29703e6ed2498b23b020138345b19dec7c7
Shlomo Y <shlomo@techtio.io>
2025-08-19 10:15:49 +0300

    charts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
+import AppBarChart from "@/components/AppBarChart";
+import AppAreaChart from "@/components/AppAreaChart";
+import AppPieChart from "@/components/AppPieChart";
+
 const Homepage = () => {
     return (
         <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
-            <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">Test</div>
+            <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+                <AppBarChart />
+            </div>
             <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+            <div className="bg-primary-foreground p-4 rounded-lg">
+                <AppPieChart />
+            </div>
             <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
-            <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
-            <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">Test</div>
+            <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+                <AppAreaChart />
+            </div>
             <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
         </div>
     );
