4a76b98354bfd3fbe9d0471eb08e38f9bcba6005
Shlomo Y <shlomo@techtio.io>
2025-08-19 08:32:24 +0300

    grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
-
 const Homepage = () => {
     return (
-        <div className="">
-
+        <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
+            <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">Test</div>
+            <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+            <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+            <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+            <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">Test</div>
+            <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
         </div>
     );
 };
