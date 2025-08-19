63a68e1df7ac6663f0578b88256adea2442ce92a
Shlomo Y <shlomo@techtio.io>
2025-08-19 10:58:50 +0300

    cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import AppBarChart from "@/components/AppBarChart";
 import AppAreaChart from "@/components/AppAreaChart";
 import AppPieChart from "@/components/AppPieChart";
+import CardList from "@/components/CardList";
 
 const Homepage = () => {
     return (
@@ -8,7 +9,9 @@ const Homepage = () => {
             <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
                 <AppBarChart />
             </div>
-            <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+            <div className="bg-primary-foreground p-4 rounded-lg">
+                <CardList title="Latest Transactions" />
+            </div>
             <div className="bg-primary-foreground p-4 rounded-lg">
                 <AppPieChart />
             </div>
@@ -16,7 +19,9 @@ const Homepage = () => {
             <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
                 <AppAreaChart />
             </div>
-            <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+            <div className="bg-primary-foreground p-4 rounded-lg">
+                <CardList title="Popular Content" />
+            </div>
         </div>
     );
 };
